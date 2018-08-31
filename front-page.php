b4140e9c37f6223a110d9c5430429d6da732314a
augustxp <xpalmer00@gmail.com>
2018-08-30 22:51:46 -0400

    changed front page

--- front-page.php
+++ front-page.php
@@ -4,12 +4,22 @@
 
     <div class="text-center l_title">The Unending Rain</div>
     <div class="text-center l_nav fade-in">
-      <p class="l_prologue"><a href="category/the-unending-rain/?order=asc">Prologue</a></p>
+      <p class="l_prologue"><a href="category/the-unending-rain/?order=asc">Chapter 1</a></p>
       <p class="l_star">&#x2605;</p>
       <p class="l_latest"><a href="category/the-unending-rain/">Latest</p></p>
 
     </div>
 
+    <div class="footer container">
+        <nav class="nav-links float-right">
+          <?php
+          $args = array(
+            'theme_location' => 'secondary'
+          );
+          ?>
+          <?php wp_nav_menu($args); ?>
+        </nav>
+    </div>
   </body>
 
 
